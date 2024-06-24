48706ddfaa97424f9ae981dd10dbb270fcdf3358
ToddLiao <toddliao.dev@gmail.com>
2024-06-24 23:33:45 +0800

    chore: add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@components': '/src/modules/components',
+      '@api': '/src/api',
+      '@pages': '/src/modules/pages',
+      '@utils': '/src/utils'
+    }
+  },
   plugins: [react()],
 })
