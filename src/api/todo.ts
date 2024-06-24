a1f5b7d01067889af3edcf5cfeacd37d2face07b
ToddLiao <toddliao.dev@gmail.com>
2024-06-25 00:01:14 +0800

    feat: add todo mockapi

---
+++ src/api/todo.ts
@@ -0,0 +1,47 @@
+import { Todo } from "../utils/types"
+
+// mock data
+const getTodoList = (): Promise<Todo[]> => {
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        {
+          todoId: "1",
+          title: 'First todo',
+          description: 'This is the first todo',
+          completed: false
+        },
+        {
+          todoId: "2",
+          title: 'Second todo',
+          description: 'This is the second todo',
+          completed: true
+        },
+        {
+          todoId: "3",
+          title: 'Third todo',
+          description: 'This is the third todo',
+          completed: false
+        },
+        {
+          todoId: "4",
+          title: 'Fourth todo',
+          description: 'This is the fourth todo',
+          completed: false
+        },
+        {
+          todoId: "5",
+          title: 'Fifth todo',
+          description: 'This is the fifth todo',
+          completed: true
+        }
+
+      ])
+    }, 1000)
+
+  })
+}
+
+
+export { getTodoList }
---
+++ src/utils/types.ts
@@ -0,0 +1,9 @@
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+  todoId: string;
+}
+
+
+export type { Todo };
