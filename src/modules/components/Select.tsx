473c1f66f956d80555815785d44288935dfd880c
ToddLiao <toddliao.dev@gmail.com>
2024-06-26 21:08:19 +0800

    feat: add Select component

---
+++ src/modules/components/Select.tsx
@@ -0,0 +1,27 @@
+import { FunctionComponent, PropsWithChildren } from "react";
+
+interface SelectProps {
+  label: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+}
+
+const Select: FunctionComponent<PropsWithChildren<SelectProps>> = (props) => {
+  const { label, value, onChange, children } = props;
+  return (
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">{label}</span>
+      </div>
+      <select
+        className="select select-bordered"
+        value={value}
+        onChange={onChange}
+      >
+        {children}
+      </select>
+    </label>
+  );
+};
+
+export default Select;
