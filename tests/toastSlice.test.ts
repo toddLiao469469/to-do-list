a5a1fece101be4c4e8dc243e0a5fbe90d10ba11f
ToddLiao <toddliao.dev@gmail.com>
2024-06-26 23:02:38 +0800

    test: add toastSlice.test

---
+++ tests/toastSlice.test.ts
@@ -0,0 +1,48 @@
+import { expect, describe, it, vi } from "vitest";
+
+import toastSlice, { addToast, deleteToast, ToastState } from "@store/slices/toastSlice";
+
+vi.useFakeTimers();
+
+const fixedId = "fixed-id";
+
+vi.mock("@reduxjs/toolkit", async (importOriginal) => {
+  const originalModule = await importOriginal<typeof import("@reduxjs/toolkit")>();
+  return {
+    ...originalModule,
+    nanoid: vi.fn(() => fixedId),
+  };
+});
+
+describe("toast reducer", () => {
+  it("should return initial state", () => {
+    expect(toastSlice(undefined, { type: "unknown" })).toEqual({
+      toasts: [],
+    });
+  });
+
+  it("should add new toast", () => {
+    const previousState = {
+      toasts: [],
+    };
+
+    const newToast = {
+      type: "success" as const,
+      message: "message",
+    };
+
+    expect(toastSlice(previousState, addToast(newToast))).toEqual({
+      toasts: [{ ...newToast, toastId: fixedId }],
+    });
+  });
+
+  it("should delete toast", () => {
+    const previousState: ToastState = {
+      toasts: [{ toastId: fixedId, type: "success", message: "message" }],
+    };
+
+    expect(toastSlice(previousState, deleteToast(fixedId))).toEqual({
+      toasts: [],
+    });
+  });
+});
